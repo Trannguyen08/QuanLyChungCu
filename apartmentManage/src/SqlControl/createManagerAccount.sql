867e594b08f7dbb4b3ba1c5ce62173adb8fe491e
Nguyen <Nguyen@LAPTOP-D2K69PD8>
2025-03-13 14:49:55 +0700

    create manager account commit 2

--- apartmentManage/src/SqlControl/createManagerAccount.sql
+++ apartmentManage/src/SqlControl/createManagerAccount.sql
@@ -2,8 +2,8 @@ use apartment;
 
 INSERT INTO users (username, password, email, role) 
 VALUES 
-('admin1', '12345', 'manager1@example.com', 'manager'),
-('admin2', 'hashed_password2', 'manager2@example.com', 'manager'),
-('admin3', 'hashed_password3', 'manager3@example.com', 'manager');
+('manager1', '$2a$12$0SjPINwZiKoMIi7m/mR/B./zNmfd6qVTGwXYZJ/8hQTCIRcaVvlVq', 'manager1@example.com', 'manager'),
+('manager2', '$2a$12$t4nel.5cXOT.4IzRauSAmuwlANLVOgT5NuL04RU/EaG7ePvxC2C6S', 'manager2@example.com', 'manager'),
+('manager3', '$2a$12$NPs2myI6ItXNqTLe11pvCu6B/p5zW22vQrlThFaqV1cKCcd0RKmHO', 'manager3@example.com', 'manager');
 
 
